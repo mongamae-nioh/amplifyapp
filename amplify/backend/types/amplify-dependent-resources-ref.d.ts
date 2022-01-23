d521f2fbe7ff69b0b2d18c126377c59755d7d3ac
mongamae-nioh <jkawaoka@gmail.com>
2022-01-23 22:53:40 +0900

    update: add api

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,13 +1,9 @@
 export type AmplifyDependentResourcesAttributes = {
-    "auth": {
-        "amplifyappdb63165b": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string"
+    "api": {
+        "amplifyapp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
         }
     }
 }
